631adc2aaffdf9e53eef59f60436a103d090fc4d
Pierry <Derungspierry15@gmail.com>
2022-05-10 20:16:12 -0300

    secound commit

--- login.php
+++ login.php
@@ -19,29 +19,112 @@
     <link href="signin.css" rel="stylesheet">
   </head>
 
-  <body class="text-center">
-    <main class="form-signin">
-      <form>
-        <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  <body>
+    <section class="vh-100 gradient-custom">
+      <div class="container py-5 h-100">
+        <div class="row justify-content-center align-items-center h-100">
+          <div class="col-12 col-lg-9 col-xl-7">
+            <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+              <div class="card-body p-4 p-md-5">
+                <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
+                <form>
 
-        <div class="form-floating">
-          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-          <label for="floatingInput">Email address</label>
-        </div>
-        <div class="form-floating">
-          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-          <label for="floatingPassword">Password</label>
-        </div>
+                  <div class="row">
+                    <div class="col-md-6 mb-4">
 
-        <div class="checkbox mb-3">
-          <a href="">
-             <button type="button" class="btn btn-light">Registre-se</button>
-          </a>
+                      <div class="form-outline">
+                        <input type="text" id="firstName" class="form-control form-control-lg" />
+                        <label class="form-label" for="firstName">First Name</label>
+                      </div>
+
+                    </div>
+                    <div class="col-md-6 mb-4">
+
+                      <div class="form-outline">
+                        <input type="text" id="lastName" class="form-control form-control-lg" />
+                        <label class="form-label" for="lastName">Last Name</label>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-6 mb-4 d-flex align-items-center">
+
+                      <div class="form-outline datepicker w-100">
+                        <input type="text" class="form-control form-control-lg" id="birthdayDate" />
+                        <label for="birthdayDate" class="form-label">Birthday</label>
+                      </div>
+
+                    </div>
+                    <div class="col-md-6 mb-4">
+
+                      <h6 class="mb-2 pb-1">Gender: </h6>
+
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
+                          value="option1" checked />
+                        <label class="form-check-label" for="femaleGender">Female</label>
+                      </div>
+
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
+                          value="option2" />
+                        <label class="form-check-label" for="maleGender">Male</label>
+                      </div>
+
+                      <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
+                          value="option3" />
+                        <label class="form-check-label" for="otherGender">Other</label>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-md-6 mb-4 pb-2">
+
+                      <div class="form-outline">
+                        <input type="email" id="emailAddress" class="form-control form-control-lg" />
+                        <label class="form-label" for="emailAddress">Email</label>
+                      </div>
+
+                    </div>
+                    <div class="col-md-6 mb-4 pb-2">
+
+                      <div class="form-outline">
+                        <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
+                        <label class="form-label" for="phoneNumber">Phone Number</label>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-12">
+
+                      <select class="select form-control-lg">
+                        <option value="1" disabled>Choose option</option>
+                        <option value="2">Subject 1</option>
+                        <option value="3">Subject 2</option>
+                        <option value="4">Subject 3</option>
+                      </select>
+                      <label class="form-label select-label">Choose option</label>
+
+                    </div>
+                  </div>
+
+                  <div class="mt-4 pt-2">
+                    <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-      </form>
-    </main>
+      </div>
+    </section>
   </body>
 </html>
