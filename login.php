7f0e2738fc258b210feb15dd2e13b2f5c7d9bfb7
Pierry <Derungspierry15@gmail.com>
2022-05-10 20:21:04 -0300

    third commit

--- login.php
+++ login.php
@@ -8,15 +8,11 @@
     <meta name="generator" content="Hugo 0.88.1">
     <title>Signin Template · Bootstrap v5.1</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- Bootstrap core CSS -->
     <link href="./assets/css/login.css" rel="stylesheet">
 
-    
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
   </head>
 
   <body>
