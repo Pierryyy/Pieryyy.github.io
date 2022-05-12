50d889be9a3def0e61f34c8b7291487845c41e3f
Your Name <you@example.com>
2022-05-12 20:27:28 -0300

    six commit

--- register.php
+++ register.php
@@ -22,48 +22,46 @@
           <div class="col-12 col-lg-9 col-xl-7">
             <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
               <div class="card-body p-4 p-md-5">
-                <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Formulario de registro</h3>
+                <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Formulário de registro</h3>
                 <form>
                   <div class="row">
                     <div class="col-md-6 mb-4">
                       <div class="form-outline">
-                        <label class="form-label" for="firstName">Primeiro Nome: </label>
-                        <input type="text" id="firstName" class="form-control form-control-lg" />
+                        <input type="text" id="firstName" placeholder="Nome" class="form-control form-control-lg" />
+                        <label class="form-label" for="firstName">Nome</label>
                       </div>
                     </div>
                     <div class="col-md-6 mb-4">
                       <div class="form-outline">
-                      <label class="form-label" for="lastName">Segundo Nome: </label>
-                        <input type="text" id="lastName" class="form-control form-control-lg" />
-                     
+                        <input type="text" id="lastName" placeholder="Sobrenome" class="form-control form-control-lg" />
+                        <label class="form-label" for="lastName">Sobrenome</label>
                       </div>
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-md-6 mb-4 d-flex align-items-center">
                       <div class="form-outline datepicker w-100">
-                        <input type="text" class="form-control form-control-lg" id="birthdayDate" />
+                        <input type="text" placeholder="00/00/0000" class="form-control form-control-lg" id="birthdayDate" />
                         <label for="birthdayDate" class="form-label">Data de nascimento</label>
                       </div>
                     </div>
                     <div class="col-md-6 mb-4">
-                    <input type="text" id="form3Example8" class="form-control form-control-lg" />
-  
-  
-                    <label class="form-label" for="cpfnumber">CPF</label>
-                    </div>
+                    
+                    <input type="text" class="form-control cpf-mask" placeholder="000.000.000-00">
+                    <label for="cpfnumber" class="form-label">CPF</label>
+                  </div>
                   </div>
                   <div class="row">
                     <div class="col-md-6 mb-4 pb-2">
                       <div class="form-outline">
-                        <input type="email" id="emailAddress" class="form-control form-control-lg" />
+                        <input type="email" id="emailAddress" placeholder="seuemail@gmail.com" class="form-control form-control-lg" />
                         <label class="form-label" for="emailAddress">Email</label>
                       </div>
                     </div>
                     <div class="col-md-6 mb-4 pb-2">
                       <div class="form-outline">
-                        <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
-                        <label class="form-label" for="phoneNumber">Numero de Telefone</label>
+                        <input type="tel" id="phoneNumber" placeholder="(00) 000000000" class="form-control phoneNumber-mask" />
+                        <label class="form-label" for="phoneNumber">Número de Telefone</label>
                       </div>
                     </div>
                     <div class="col-md-6 mb-4">
