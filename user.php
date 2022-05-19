7de5794fec660bac720b0367d2559d7e8775ae5f
Pierry <Derungspierry15@gmail.com>
2022-05-19 20:19:12 -0300

    twelve commit

--- user.php
+++ user.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title>Signin Template · Bootstrap v5.1</title>
+    <title>Tela de Usuario</title>
     <link href="./assets/css/login.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -63,7 +63,8 @@
                             <div class="col-md-6"><label class="labels">Data de Nascimento</label>
                             <input type="text" name="birthdayDate" placeholder="00/00/0000" class="form-control form-control-lg" id="birthdayDate" /></div>
                         </div>
-                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Salvar Perfil</button></div>
+                        <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Salvar Perfil</button> <button class="btn btn-primary profile-button" type="button">Deletar Perfil</button></div>
+                      
                     </div>
                 </div>
                 <div class="col-md-4">
