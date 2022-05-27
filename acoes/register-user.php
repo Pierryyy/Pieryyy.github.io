66e6a313db07b701af97609227be18ad103a02c3
Pierry <Derungspierry15@gmail.com>
2022-05-26 21:40:35 -0300

    database

--- acoes/register-user.php
+++ acoes/register-user.php
@@ -6,11 +6,21 @@
     //verifica se o usuario apertou no botao de cadastrar
     if(isset($_POST['bt_cadastrar'])){
         //pegar os dados e inserir no banco
-        
-        $nome = mysqli_real_escape_string($con, $_POST['nome']);
-        $senha = mysqli_real_escape_string($con, $_POST['senha']);
 
-        $sql = "INSERT INTO users (nome, senha) VALUES ('$nome', '$senha')";
+        $emailAddress = mysqli_real_escape_string($con, $_POST['emailAddress']);
+        $firstName = mysqli_real_escape_string($con, $_POST['firstName']);
+        $password = md5(mysqli_real_escape_string($con, $_POST['password']));
+        $birthdayDate = (mysqli_real_escape_string($con, $_POST['birthdayDate']));
+        $cpf = (mysqli_real_escape_string($con, $_POST['cpf']));
+        $phoneNumber = (mysqli_real_escape_string($con, $_POST['phoneNumber']));
+        $cep = mysqli_real_escape_string($con, $_POST['cep']);
+        $estate = mysqli_real_escape_string($con, $_POST['estate']);
+        $city = mysqli_real_escape_string($con, $_POST['city']);
+        $street = mysqli_real_escape_string($con, $_POST['street']);
+        $houseNumber = mysqli_real_escape_string($con, $_POST['houseNumber']);
+
+
+        $sql = "INSERT INTO users (firstName, password, emailAddress, birthdayDate, cpf, phoneNumber, cep, estate, city, street, houseNumber) VALUES ('$firstName', '$password', '$emailAddress', '$birthdayDate', '$cpf', '$phoneNumber', '$cep', '$estate', '$city', '$street', '$houseNumber')";
 
         //verificar sucesso
         if(mysqli_query($con, $sql)){
